ba9c7fb2eae1de056dc500c24f3597abf634b9a9
SEEANSOUL <83467617+SEEANSOUL@users.noreply.github.com>
2025-10-08 18:38:53 +0300

    Update const firebaseConfig = {.js

--- Adisuon/const firebaseConfig = {.js
+++ Adisuon/const firebaseConfig = {.js
@@ -1,8 +1,1 @@
- const firebaseConfig = {
-      apiKey: "AIzaSyCwpKUpII22XkWRIf_3UehUYUyZQfGD90Y",
-      authDomain: "adisyon-f9ea2.firebaseapp.com",
-      projectId: "adisyon-f9ea2",
-      storageBucket: "adisyon-f9ea2.firebasestorage.app",
-      messagingSenderId: "1024510526279",
-      appId: "1:1024510526279:web:07193ebf6ec6f6e79d0262"
-    };
+
